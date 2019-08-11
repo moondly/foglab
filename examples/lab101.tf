5a6d25b49b9e7aa0d36e784e867c5b5b838f3b5b
moondly <moondly@protonmail.com>
2019-08-11 10:40:22 +0200

    Adapt terraform definition files to new version

--- examples/lab101.tf
+++ examples/lab101.tf
@@ -1,4 +1,6 @@
 provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
 }
 
 # Example Ubuntu LXD
@@ -7,30 +9,29 @@ resource "lxd_container" "node1" {
   image     = "ubuntu:18.04"
   ephemeral = false
   profiles  = ["default"]
-  limits {
-    cpu = "1"
+  limits = {
+    cpu    = "1"
     memory = "1GB"
   }
 
-  config {
-    user.access_interface = "eth0"  # Default interface to be reported in terraform show
+  config = {
+    "user.access_interface" = "eth0" # Default interface to be reported in terraform show
   }
 
   # Using a fixed IP
   device {
     name = "eth0"
     type = "nic"
-    properties {
-      nictype = "bridged"
-      parent = "lxdbr0"
-      ipv4.address = "192.168.55.180"
+    properties = {
+      nictype      = "bridged"
+      parent       = "lxdbr0"
+      "ipv4.address" = "192.168.55.180"
     }
   }
 
   provisioner "local-exec" {
     command = ". /opt/foglab/scripts/lxc_utils.sh && prepareUbuntu ${lxd_container.node1.name}"
   }
-  
 }
 
 # Example CentOs LXD
@@ -39,13 +40,13 @@ resource "lxd_container" "node2" {
   image     = "images:centos/7"
   ephemeral = false
   profiles  = ["default"]
-  limits {
-    cpu = "1"
+  limits = {
+    cpu    = "1"
     memory = "1GB"
   }
 
-  config {
-    user.access_interface = "eth0"
+  config = {
+    "user.access_interface" = "eth0"
   }
 
   provisioner "local-exec" {
