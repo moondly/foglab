6c0cfb5432b9f5eb1c5e5b7933afacd94746a52c
moondly <moondly@protonmail.com>
2019-10-26 21:09:05 +0200

    Create droplet in phases

--- scripts/dropctl.py
+++ scripts/dropctl.py
@@ -43,27 +43,60 @@ def init(args):
 
 def apply(args):
   currDir = os.getcwd()
-  playbook = LOCAL_ACTIONS_DIR + "/droplet.yml"
   name = args.name
+  phases = args.phases
+  
+  print("Phases to run: %r" % phases)
+
+  initTasks = os.path.join(os.sep,LOCAL_ACTIONS_DIR,"droplet.yml")
   requirements = os.path.join(os.sep,DROPLET_ROLES_DIR,name,"requirements.yml")
-  mainFile = os.path.join(os.sep,DROPLET_ROLES_DIR,name,"apply.yml")
+  preLocal = os.path.join(os.sep,DROPLET_ROLES_DIR,name,"preLocal.yml")
+  mainFile = os.path.join(os.sep,DROPLET_ROLES_DIR,name,"provision.yml")
+  posLocal = os.path.join(os.sep,DROPLET_ROLES_DIR,name,"posLocal.yml")
+  
   deployToDir = os.path.join(os.sep,currDir,name)
   rolesPath = ["~/.ansible/roles","/usr/share/ansible/roles","/etc/ansible/roles", DROPLET_ROLES_DIR]
 
   print("Creating droplet: %r" % name)
 
-  # Install prereq
-  if os.path.isfile(requirements):
-    rsr("ansible-galaxy install -r %r" % (requirements))
   # Local actions
-  extra = "{'droplet_name': %r, 'dir': %r, 'droplet_dir': %r}" % (name, currDir, DROPLET_DIR)
-  rsr("ansible-playbook %r --extra-vars %r" % (playbook, extra))
+  if 'all' in phases or 'init' in phases:
+    print("Init and infra phases")
+    extra = "{'droplet_name': %r, 'dir': %r, 'droplet_dir': %r}" % (name, currDir, DROPLET_DIR)
+    rsr("ansible-playbook %r --extra-vars %r" % (initTasks, extra))
+
+  # Install prereq
+  if 'all' in phases or 'prereq' in phases:
+    if os.path.isfile(requirements):
+      rsr("ansible-galaxy install -r %r" % (requirements))
+
+  # Run preLocal prereq
+  if 'all' in phases or 'prelocal' in phases:
+    if os.path.isfile(preLocal):
+      print("PreLocal phase")
+      extra = "{'drop_home': %r, 'drop_name': %r}" % (deployToDir, name)
+      rsr("ansible-playbook %r --extra-vars %r" % (preLocal, extra))
+  
   # Infra deployment
-  rsr("cd %r && fogctl vm -a --approve" % (deployToDir))
-  # Remote actions
-  rsr("cd %r && ANSIBLE_ROLES_PATH=%r ansible-playbook -i .hosts %r -u root " % (deployToDir,":".join(rolesPath),mainFile))
+  if 'all' in phases or 'infra' in phases:
+    rsr("cd %r && fogctl vm -a --approve" % (deployToDir))
+
+  # Remote provisioning
+  if 'all' in phases or 'provisioning' in phases:
+    rsr("cd %r && ANSIBLE_ROLES_PATH=%r ansible-playbook -i .hosts %r -u root " % (deployToDir,":".join(rolesPath),mainFile))
+
+  # Run posLocal prereq
+  if 'all' in phases or 'poslocal' in phases:
+    if os.path.isfile(posLocal):
+      print("PosLocal phase")
+      extra = "{'drop_home': %r, 'drop_name': %r}" % (deployToDir, name)
+      rsr("ansible-playbook %r --extra-vars %r" % (posLocal, extra))
+
   # Info
-  rsr("cd %r && cat %r" % (deployToDir, "README"))
+  if 'all' in phases or 'readme' in phases:
+    rsr("cd %r && cat %r" % (deployToDir, "README"))
+  
+  print("Done!")
 
 def destroy(args):
   currDir = os.getcwd()
@@ -96,6 +129,7 @@ parser_swap.set_defaults(func=init)
 parser_create = subparsers.add_parser('apply')
 parser_create.add_argument('name', type=str, help='The droplet name')
 parser_create.add_argument('-i', choices=['ubuntu:18.04', 'centos/7'], default='ubuntu:18.04', help='The image to use. Default: ubuntu:18.04. TODO: detect from lab.tf!!')
+parser_create.add_argument('--phases', nargs='+', choices=['all', 'init', 'prereq', 'prelocal', 'infra', 'poslocal', 'readme'], default=['all'], help='Phases to run')
 parser_create.set_defaults(func=apply)
 
 parser_destroy = subparsers.add_parser('destroy')
